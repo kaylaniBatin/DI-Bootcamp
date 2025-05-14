cb377461b27ed726964067cfe69c50e22a760282
kaylaniBatin <evanskaylani@gmail.com>
2025-05-14 13:39:51 +0300

    comment

--- W1/D3/Exercisexp.py
+++ W1/D3/Exercisexp.py
@@ -19,3 +19,36 @@ for member, age in family.items():
 print(f"\nTotal cost: ${total_cost}")
 
 #Exercise 3 zara
+brand = {
+    "name": "Zara",
+    "creation_date": 1975,
+    "creator_name": "Amancio Ortega Gaona",
+    "type_of_clothes": ["men", "women", "children", "home"],
+    "international_competitors": ["Gap", "H&M", "Benetton"],
+    "number_stores": 7000,
+    "major_color": {
+        "France": "blue",
+        "Spain": "red",
+        "US": ["pink", "green"]
+    }
+}
+brand["number_stores"] = 2
+print(f"Zara's clients include {', '.join(brand['type_of_clothes'])}.")
+brand["country_creation"] = "Spain"
+if "international_competitors" in brand:
+    brand["international_competitors"].append("Desigual")
+del brand["creation_date"]
+print("Last international competitor:", brand["international_competitors"][-1])
+print("Major colors in the US:", ", ".join(brand["major_color"]["US"]))
+print("Number of keys in the dictionary:", len(brand))
+print("Keys in the dictionary:", list(brand.keys()))
+
+#Exercise 4
+users = ["Mickey", "Minnie", "Donald", "Ariel", "Pluto"]
+char_to_index = {user: index for index, user in enumerate(users)}
+print("Character to Index:", char_to_index)
+index_to_char = {index: user for index, user in enumerate(users)}
+print("Index to Character:", index_to_char)
+sorted_users = sorted(users)
+sorted_char_to_index = {user: index for index, user in enumerate(sorted_users)}
+print("Alphabetically Sorted Character to Index:", sorted_char_to_index)
---
+++ W1/D4/functions.py
@@ -0,0 +1,38 @@
+#functions
+#syntax
+
+# def func_name():
+#     '''prints a phrase on the console''' #dog strings explains the function
+#     print('I am a function')
+
+# func_name()
+# print(print.__doc__)
+
+#create a function called greetings that prints 'Hello' in your mother tongue
+def greetings(language, user_name):
+    if language == "EN":
+        print(f'Hello{user_name}')
+    elif language == "RU":
+        print(f'Priviet {user_name}')
+    else:
+        print('Undefined language')
+greetings('RU', 'Ilia')
+
+#Key word arguements   
+greetings(user_name = 'Ilia', language = 'RU')
+
+#Create a function called country_info that receives a country as an argument 
+# and print the capital of that country. Make the country name arguement default
+# Baku 
+
+def country_info(country_name = Naboo):
+    if country_name == "France":
+    print(f'The capital of {country_info} is Paris')
+elif country_name == 'Italy':
+    print(f'the capital of {country_name} is Rome')
+    else:
+        print('Country not found')
+country_info('Naboo')
+
+
+
